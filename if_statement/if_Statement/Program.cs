64de513d79fdd532ebdbe9dc738ab4bcdf71adec
aburnley88 <68288722+aburnley88@users.noreply.github.com>
2020-07-18 05:07:15 -0400

    Add files via upload
    my commit

---
+++ if_statement/if_Statement/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace if_Statement
+{
+    class Program
+        //a simplet program demonstrating an if statement taken from MTA 98-361
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 10;
+            int number2 = 20;
+            if (number2 > number1)
+            {
+                Console.WriteLine("number2 is greater than number1");
+            }
+        }
+    }
+}
