b79d56f836e108fb0a3dfd860d63cf4a603befd9
aburnley88 <68288722+aburnley88@users.noreply.github.com>
2020-07-18 07:57:17 -0400

    Add files via upload
    Lesson 2 exercises

---
+++ Lesson02/Rectangle/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Net.NetworkInformation;
+
+namespace Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect1 = new Rectangle
+            { Length = 10, Width = 20 };
+            Rectangle rect2 = new Rectangle
+            { Length = 100, Width = 200 };
+            Console.WriteLine(rect1.CompareTo(rect2));
+           
+            Triangle t = new Triangle();
+            t.Draw();
+            Polygon p = t;
+            p.Draw();
+
+            List<Polygon> polygons = new List<Polygon>();
+            polygons.Add(new Polygon());
+            polygons.Add(new Rectangle());
+            polygons.Add(new Triangle());
+            
+
+
+        }
+       
+       class Polygon
+        {
+            public virtual void Draw()
+            {
+                Console.WriteLine("Drawing: Polygon");
+            }
+
+        }
+        
+        class Rectangle: Polygon, IComparable
+        {
+            
+            public double Length { get; set; }
+            public double Width { get; set; }
+            public override void Draw()
+            {
+                Console.WriteLine("Drawing: Rectangle");
+            }
+            public double GetArea()
+            {
+                return Length * Width;
+            }
+            public int CompareTo(object obj)
+            {
+                if (obj == null)
+                    return 1;
+                if (!(obj is Rectangle))
+                    throw new ArgumentException();
+                Rectangle target = (Rectangle)obj;
+                double diff = this.GetArea() - target.GetArea();
+                if (diff == 0)
+                    return 0;
+                else if (diff > 0)
+                    return 1;
+                else return -1;
+            }
+                     
+          
+        }
+        class Triangle: Polygon
+        {
+            public new void Draw()
+            {
+                Console.WriteLine("Drawing: Triangle");
+            }
+        }
+        
+    }
+}
